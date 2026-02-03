6c8f18e10d0671e724451da3eeff2e1a50979fc1
Augusto Prestes <57774809+AugustoPrestes@users.noreply.github.com>
2026-02-03 17:43:41 +0000

    Iniciando a manipulação do link da API

--- app.py
+++ app.py
@@ -1,13 +1,30 @@
-# imports
+#%% imports
 import requests
+import pandas as pd
+import json
+from datetime import datetime, date
 
-# Parametros 
-date_start = ''
+#%% Parametros 
+
+# Suparametros
+dia = 10
+mes = 12
+ano = 2020
+
+date_start = f'{ano}-{mes}-{dia}'
 date_end = ''
-count = ''
+count = int(100)
 
-url_path = "https://old.west.albion-online-data.com/api/v2/stats/Gold?date=2025-10-22&end_date=2026-01-22&count=1"
+url_path = f"https://old.west.albion-online-data.com/api/v2/stats/Gold?date={date_start}&end_date={date_end}&count={count}"
 
+response = requests.get(url=url_path)
+data = response.text
 
-r = requests.get(url=url_path)
-print(r.text)
+#%% Passando o json para um dataframe
+df = pd.json_normalize(data, 'abc')
+
+print(df)
+
+# %%
+data
+# %%
